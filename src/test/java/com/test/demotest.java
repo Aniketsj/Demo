c5592d8d3a0b9352aec7ff27c1aa903b91a5a68b
aniket <Aniket.Jadhav@ezest.local>
2020-02-13 14:43:53 +0530

    test

---
+++ src/test/java/com/test/demotest.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demotest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","E:\\eclips projects\\Demo\\Driver\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+
+		String title= driver.getTitle();
+		System.out.println(title);
+		driver.close();
+	}
+
+}
