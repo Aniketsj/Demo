8ac8b9e3e8e5d63c1ffa28f233bcb65238a4d69b
Aniket <Aniket.Jadhav@ezest.local>
2020-02-17 18:53:24 +0530

    test

--- src/test/java/com/test/demo.java
+++ src/test/java/com/test/demo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class demotest {
+public class demo {
 
 	public static void main(String[] args) {
 		
